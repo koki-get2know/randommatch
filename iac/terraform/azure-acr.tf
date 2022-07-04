f4595ed325c75280230d1c66c27b6b4fc1c1a422
Ivan Tmib <tmibkage@gmail.com>
2022-07-04 08:48:12 +0200

    name changing

--- iac/terraform/azure-acr.tf
+++ iac/terraform/azure-acr.tf
@@ -1,4 +1,4 @@
-resource "azurerm_container_registry" "acr" {
+resource "azurerm_container_registry" "container_registry" {
   name                = local.container_registry_name
   location            = azurerm_resource_group.aks.location
   resource_group_name = azurerm_resource_group.aks.name
