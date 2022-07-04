069db7320527fb7bc15743a03543a5c65ee98f02
Ivan Tmib <tmibkage@gmail.com>
2022-07-04 08:43:37 +0200

    terraform format

--- iac/terraform/azure-aks.tf
+++ iac/terraform/azure-aks.tf
@@ -30,8 +30,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_role_assignment" "aks_to_acr_role" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
 }
