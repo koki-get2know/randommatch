a3269315634a39703254aa081b550acdf2d9d72d
Ivan Tmib <tmibkage@gmail.com>
2022-07-22 01:35:49 +0200

    format

--- application/core/matcher/graph_test.go
+++ application/core/matcher/graph_test.go
@@ -1,40 +1,38 @@
 package matcher
 
 import (
-    "testing"
-    
-    
+	"testing"
 )
 
 var g UserGraph
 
 func fillGraph() {
-    nA := User{"1"}
-    nB := User{"2"}
-    nC := User{"3"}
-    nD := User{"4"}
-    nE := User{"5"}
-    nF := User{"6"}
-    g.AddUser(&nA)
-    g.AddUser(&nB)
-    g.AddUser(&nC)
-    g.AddUser(&nD)
-    g.AddUser(&nE)
-    g.AddUser(&nF)
+	nA := User{"1"}
+	nB := User{"2"}
+	nC := User{"3"}
+	nD := User{"4"}
+	nE := User{"5"}
+	nF := User{"6"}
+	g.AddUser(&nA)
+	g.AddUser(&nB)
+	g.AddUser(&nC)
+	g.AddUser(&nD)
+	g.AddUser(&nE)
+	g.AddUser(&nF)
 
-    g.AddEdge(&nA, &nB)
-    g.AddEdge(&nA, &nC)
-    g.AddEdge(&nB, &nE)
-    g.AddEdge(&nC, &nE)
-    g.AddEdge(&nE, &nF)
-    g.AddEdge(&nD, &nA)
+	g.AddEdge(&nA, &nB)
+	g.AddEdge(&nA, &nC)
+	g.AddEdge(&nB, &nE)
+	g.AddEdge(&nC, &nE)
+	g.AddEdge(&nE, &nF)
+	g.AddEdge(&nD, &nA)
 }
 
 func TestAdd(t *testing.T) {
-    fillGraph()
-    g.String()
-    //n := User{"1"}
-    //g.RemoveUser(&n)
-    //g.String()
-    
+	fillGraph()
+	g.String()
+	//n := User{"1"}
+	//g.RemoveUser(&n)
+	//g.String()
+
 }
--- application/core/matcher/random_test.go
+++ application/core/matcher/random_test.go
@@ -1,44 +1,38 @@
 package matcher
 
 import (
-    "fmt"
-    "testing"
-    
+	"fmt"
+	"testing"
 )
 
-
 func TestRandomChoices(t *testing.T) {
-   //id := []string{"2", "5", "6", "8", "10", "12", "24", "25"}
-    g.String()
-    constraint := []string{"deja vu"}
-    matching := RandomChoices(&g,2, constraint)
-    fmt.Printf("Match of %d: [" ,len(matching.matched))
-    for i := 0; i < len(matching.matched); i++{
-       fmt.Printf("%s,",matching.matched[i].String())
-    }
-    
-    fmt.Printf("]")
-}
+	//id := []string{"2", "5", "6", "8", "10", "12", "24", "25"}
+	g.String()
+	constraint := []string{"deja vu"}
+	matching := RandomChoices(&g, 2, constraint)
+	fmt.Printf("Match of %d: [", len(matching.matched))
+	for i := 0; i < len(matching.matched); i++ {
+		fmt.Printf("%s,", matching.matched[i].String())
+	}
 
+	fmt.Printf("]")
+}
 
 func TestMatcher(t *testing.T) {
-   //id := []string{"2", "5", "6", "8", "10", "12", "24", "25"}
-    g.String()
-    constraint := []string{"deja vu"}
-    matching := Matcher(&g,2,constraint)
-    
-    for j := 0; j < len(matching); j++{ 
-      fmt.Printf("Match : [" )
-      for i := 0; i < len(matching[j].matched); i++{
-         fmt.Printf("%s,",matching[j].matched[i].String())
-      }
-    
-      fmt.Printf("]")
-      fmt.Println("")
-      
-    }
-    g.String()
+	//id := []string{"2", "5", "6", "8", "10", "12", "24", "25"}
+	g.String()
+	constraint := []string{"deja vu"}
+	matching := Matcher(&g, 2, constraint)
+
+	for j := 0; j < len(matching); j++ {
+		fmt.Printf("Match : [")
+		for i := 0; i < len(matching[j].matched); i++ {
+			fmt.Printf("%s,", matching[j].matched[i].String())
+		}
+
+		fmt.Printf("]")
+		fmt.Println("")
+
+	}
+	g.String()
 }
-
-
-
