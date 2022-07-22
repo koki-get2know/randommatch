02088c494fba80266291fe3bd0894e10e67af45d
Ivan Tmib <tmibkage@gmail.com>
2022-07-23 00:25:42 +0200

    update

--- application/ui/src/environments/environment.prod.ts
+++ application/ui/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   redirectUri: 'https://koki.sheno.ca',
   clientId: 'c6d06e75-6f10-42a0-ab0e-55e9bb8c64fe',
   authority: 'https://login.microsoftonline.com/4cab010c-fa07-44f7-bc69-561184a9fb8e',
-  serverBaseUrl: 'http://back-end',
+  serverBaseUrl: '/api',
 };
