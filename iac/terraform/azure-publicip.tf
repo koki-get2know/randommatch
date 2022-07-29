69307a2d7a1aafefbddc698fefb5162727afebb4
Ivan Tmib <tmibkage@gmail.com>
2022-07-29 16:53:57 +0200

    typo

--- iac/terraform/azure-publicip.tf
+++ iac/terraform/azure-publicip.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "aks" {
   name                = local.aks_public_ip
-  resource_group_name = azurerm_resource_group.aks.node_resource_group
+  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
   location            = azurerm_resource_group.aks.location
   allocation_method   = "Static"
   sku                 = "Standard"
