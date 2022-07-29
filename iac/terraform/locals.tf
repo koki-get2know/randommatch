e03e66ee1f79072986edfffc0ca68ec5e0f3caa8
ivantchomgue <tmibkage@gmail.com>
2022-07-29 05:18:57 +0200

    Update locals.tf

--- iac/terraform/locals.tf
+++ iac/terraform/locals.tf
@@ -2,7 +2,7 @@ locals {
   resources_prefix             = var.resources_prefix != null ? var.resources_prefix : "${local._default.name_prefix}"
   location                     = var.location
   resource_group_name          = "${local.resources_prefix}aksrg"
-  container_registry_name      = "${local.resources_prefix}akscr"
+  container_registry_name      = "${local.resources_prefix}2akscr"
   log_analytics_workspace_name = "${local.resources_prefix}aksmonitor"
   log_analytics_workspace_sku  = "PerGB2018"
   aks_cluster_name             = "${local.resources_prefix}aksk8s"
