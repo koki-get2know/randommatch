d0bb717dbd32d920e44b6558e5c1bdda53f5fc97
ivantchomgue <tmibkage@gmail.com>
2022-08-10 09:32:59 +0200

    Update bearer-interceptor.service.ts

--- application/ui/src/app/http-interceptors/bearer-interceptor.service.ts
+++ application/ui/src/app/http-interceptors/bearer-interceptor.service.ts
@@ -5,35 +5,34 @@ import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { appConstants } from '../constants';
 
-@Injectable( {
+@Injectable({
   providedIn: 'root'
-} )
+})
 export class BearerInterceptor implements HttpInterceptor {
 
-  constructor ( private authService: MsalService ) { }
+  constructor(private authService: MsalService) { }
 
-  intercept ( req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
-    if ( req.url.startsWith( appConstants.microsoftGraph ) ) {
-      return next.handle( req );
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.startsWith(appConstants.microsoftGraph)) {
+      return next.handle(req);
     }
     return this.getAuthorizationHeader()
-      .pipe(
-        switchMap( idToken => {
-          const authReq = req.clone( {
-            setHeaders: {
-              Authorization: `Bearer ${ idToken }`,
-            }
-          } );
-          return next.handle( authReq );
-        } )
-      );
+              .pipe(
+                switchMap(idToken => {
+                  const authReq = req.clone({
+                    setHeaders: {
+                      Authorization: `Bearer ${idToken}`,
+                    }});
+                    return next.handle(authReq);
+                })
+              );
   }
 
-  private getAuthorizationHeader (): Observable<string> {
-    return this.authService.acquireTokenSilent( {
-      scopes: appConstants.scopes,
-      account: this.authService.instance.getAllAccounts()[ 0 ],
+  private getAuthorizationHeader() : Observable<string> {
+    return this.authService.acquireTokenSilent({
+      scopes: appConstants.scopes, 
+      account: this.authService.instance.getAllAccounts()[0],
       forceRefresh: false
-    } ).pipe( map( authResult => authResult.idToken ) );
+    }).pipe(map(authResult => authResult.idToken));
   }
 }
