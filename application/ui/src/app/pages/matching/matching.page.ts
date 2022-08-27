449803f3fc15ef5e2a7f5bb2fc69825be259927f
Ivan Tmib <tmibkage@gmail.com>
2022-08-27 23:34:05 +0200

    lifecycle empty

--- application/ui/src/app/pages/matching/matching.page.ts
+++ application/ui/src/app/pages/matching/matching.page.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-matching",
   templateUrl: "./matching.page.html",
   styleUrls: ["./matching.page.scss"],
 })
-export class MatchingPage implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class MatchingPage {}
