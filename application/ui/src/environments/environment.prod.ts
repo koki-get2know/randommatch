1a5860a9485808a371577ee299bbf55653ca5842
Ivan Tmib <tmibkage@gmail.com>
2022-08-28 23:49:14 +0200

    app registration

--- application/ui/src/environments/environment.prod.ts
+++ application/ui/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   redirectUri: "https://koki.sheno.ca",
-  clientId: "60ed6919-1d88-4ced-8ce9-66f5dd5a540e",
+  clientId: "50afc41f-4647-4787-9d61-6c8bec34091c",
   authority:
     "https://login.microsoftonline.com/5806938e-ea7d-4345-85fb-6239156b78d6",
   serverBaseUrl: "/api",
--- application/ui/src/environments/environment.ts
+++ application/ui/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   redirectUri: "http://localhost:4200",
-  clientId: "60ed6919-1d88-4ced-8ce9-66f5dd5a540e",
+  clientId: "50afc41f-4647-4787-9d61-6c8bec34091c",
   authority:
     "https://login.microsoftonline.com/5806938e-ea7d-4345-85fb-6239156b78d6",
   serverBaseUrl: "http://localhost:8080",
