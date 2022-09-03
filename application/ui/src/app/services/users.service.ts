cf88358fbeb47c24f47693293683ae3ff36f0758
Ivan Tmib <tmibkage@gmail.com>
2022-09-03 08:06:51 +0200

    wait load before showing empty result

--- application/ui/src/app/services/users.service.ts
+++ application/ui/src/app/services/users.service.ts
@@ -151,7 +151,7 @@ export class UsersService {
   }
 
   // https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript/47593316#47593316
-  private cyrb128(str: string) {
+  /* private cyrb128(str: string) {
     let h1 = 1779033703,
       h2 = 3144134277,
       h3 = 1013904242,
@@ -182,7 +182,7 @@ export class UsersService {
       t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
       return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
     };
-  }
+  }*/
 
   generateAvatarSvg(seed: string): string /*SafeHtml*/ {
     /*const lorem = new LoremIpsum();
