8f16a4ec7fe159a064a230486b6deba87f35c167
Ivan Tmib <tmibkage@gmail.com>
2022-09-05 00:42:09 +0200

    chart

--- application/ui/src/app/pages/statistics/statistics.page.ts
+++ application/ui/src/app/pages/statistics/statistics.page.ts
@@ -1,35 +1,44 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import Chart from 'chart.js/auto';
-import { formatDate } from '@angular/common';
-import { MatchingStat, MatchingStatService } from '../../services/matching-stat.service';
-import { TranslateService } from '@ngx-translate/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import Chart from "chart.js/auto";
+import { formatDate } from "@angular/common";
+import {
+  MatchingStat,
+  MatchingStatService,
+} from "../../services/matching-stat.service";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-statistics',
-  templateUrl: './statistics.page.html',
-  styleUrls: ['./statistics.page.scss'],
+  selector: "app-statistics",
+  templateUrl: "./statistics.page.html",
+  styleUrls: ["./statistics.page.scss"],
 })
 export class StatisticsPage implements OnInit {
-
-  @ViewChild('chartCanvas') chartCanvas : ElementRef;
-  data : any = [];
-  canvasChart : Chart;
+  @ViewChild("chartCanvas") chartCanvas: ElementRef;
+  data: any = [];
+  canvasChart: Chart;
 
   matchingstatistics: MatchingStat[] = [];
-  connexionStat: string; emailSentStat: string; emailFailedStat: string;
+  connectionsGenerated: string;
+  conversationsWellTriggered: string;
+  conversationsNotSent: string;
   numUsers: boolean = false;
-  numgroups = 0;
+  totalNumConnections = 0;
   numconves = 0;
   numfailed = 0;
   numMatchingSection = 0;
 
-  constructor(private matchingstatService: MatchingStatService, public translate: TranslateService) { }
+  constructor(
+    private matchingstatService: MatchingStatService,
+    public translate: TranslateService
+  ) {}
 
   ngOnInit() {
-    this.connexionStat = this.translate.instant('NUM_CONNEXIONS');
-    this.emailSentStat = this.translate.instant('EMAIL_SENT');
-    this.emailFailedStat = this.translate.instant('EMAIL_FAILED');
+    this.connectionsGenerated = this.translate.instant(
+      "NUM_CONNECTIONS_GENERATED"
+    );
+    this.conversationsWellTriggered =
+      this.translate.instant("NUM_CONVERSATION");
+    this.conversationsNotSent = this.translate.instant("NUM_FAILED");
     this.getMathingStats();
   }
 
@@ -40,64 +49,79 @@ export class StatisticsPage implements OnInit {
     let labels = [];
 
     this.matchingstatService.getMatchingStats().subscribe((matchingStats) => {
-      if(matchingStats != null) {
+      if (matchingStats != null) {
         this.numMatchingSection = matchingStats.length;
 
-        this.matchingstatistics = matchingStats
-        for(const mathingStat of matchingStats) {
-          labels.push(formatDate(mathingStat.createdAt, "MMM d, y", "en" /**navigator.language.split("-")[0] ||  */))
-          numgroups.push(mathingStat.numGroups)
-          numemails.push(mathingStat.numConversations)
-          numfailed.push(mathingStat.numFailed)
-          this.numconves += mathingStat.numConversations
-          this.numfailed += mathingStat.numFailed 
+        this.matchingstatistics = matchingStats;
+        for (const mathingStat of matchingStats) {
+          labels.push(
+            formatDate(
+              mathingStat.createdAt,
+              "MMM d, y hh:mm:ss",
+              "en" /**navigator.language.split("-")[0] ||  */
+            )
+          );
+          numgroups.push(mathingStat.numGroups);
+          numemails.push(mathingStat.numConversations);
+          numfailed.push(mathingStat.numFailed);
+          this.numconves += mathingStat.numConversations;
+          this.numfailed += mathingStat.numFailed;
+          this.totalNumConnections += mathingStat.numGroups;
         }
         this.data = {
           labels: labels,
-          datasets: [{
-            label: this.connexionStat,
-            data: numgroups,
-            backgroundColor: 'rgba(153, 102, 255)',
-            borderColor: 'rgb(153, 102, 255)',
-            borderWidth: 1
-          },
-          {
-            label: this.emailSentStat,
-            data: numemails,
-            backgroundColor: 'rgba(75, 192, 192)',
-            borderColor: 'rgb(75, 192, 192)',
-            borderWidth: 1
-          },
-          {
-            label: this.emailFailedStat,
-            data: numfailed,
-            backgroundColor: 'rgba(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            borderWidth: 1
-          }]
+          datasets: [
+            {
+              label: this.connectionsGenerated,
+              data: numgroups,
+              backgroundColor: "rgba(56,128,255,1)",
+              borderWidth: 1,
+            },
+            {
+              label: this.conversationsWellTriggered,
+              data: numemails,
+              backgroundColor: "rgba(45,211,111,1)",
+              borderWidth: 1,
+            },
+            {
+              label: this.conversationsNotSent,
+              data: numfailed,
+              backgroundColor: "rgba(255,196,9,1)",
+              borderWidth: 1,
+            },
+          ],
         };
-        this.changeChart({detail: {
-          value : 'bar'
-        }});
+        this.changeChart({
+          detail: {
+            value: "bar",
+          },
+        });
       }
     });
   }
 
-  changeChart( event: any ) {
-    const type = event.detail.value || 'bar';
-    if ( this.canvasChart ) {
+  changeChart(event: any) {
+    const type = event.detail.value || "bar";
+    if (this.canvasChart) {
       this.canvasChart.destroy();
     }
     this.canvasChart = new Chart(this.chartCanvas.nativeElement, {
       type: type,
       data: this.data,
       options: {
-        indexAxis: 'x'
-      }
+        maintainAspectRatio: false,
+        indexAxis: "x",
+        plugins: {
+          legend: {
+            align: "start",
+            position: "top",
+          },
+        },
+      },
     });
   }
 
-  addRandom( points: any ) : number {
-    return Number(points) - Number( Math.floor((Math.random() * 100) + 1) );
+  addRandom(points: any): number {
+    return Number(points) - Number(Math.floor(Math.random() * 100 + 1));
   }
 }
