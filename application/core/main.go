5518e43c646e6faef31a79cdd26712bb8e38635e
Yannick Yomie <yynzeuhang@gmail.com>
2022-09-18 12:40:33 +0100

    [feature] Create schedule

--- application/core/main.go
+++ application/core/main.go
@@ -312,7 +312,7 @@ func main() {
 	public.POST("/albums", handler.PostAlbums)
 
 	protected := router.Group("")
-	//protected.Use(middlewares.JwtAuth())
+	protected.Use(middlewares.JwtAuth())
 	protected.POST("/matchings", generateMatchings)
 	protected.POST("/group-matchings", generateGroupMatchings)
 	protected.POST("/tag-matchings", generateMatchingByTag)
